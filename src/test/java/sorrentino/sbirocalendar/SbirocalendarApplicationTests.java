bba4ae360056aecd3366b0de8cdf061d3b7b712c
adamocopposorrentino <a.copposorrentino@iemmeconsulting.it>
2026-03-05 11:30:45 +0100

    firt commit

---
+++ src/test/java/sorrentino/sbirocalendar/SbirocalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package sorrentino.sbirocalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbirocalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
